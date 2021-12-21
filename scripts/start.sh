e8dc0c91b33d8300f4eacf9376966152804be9fd
weidingerhp <weidinger.hp@gmail.com>
2021-12-21 21:04:02 +0100

    mitigate lookup-loop issue

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,4 @@ fi
 echo Starting minecraft server with heap: HEAP ${HEAP}
 
 cd /mnt/minecraft
-java -Xmx${HEAP} -Xms${HEAP} -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -Dcom.mojang.eula.agree=true -Dfile.encoding=UTF-8 -jar /opt/minecraft/spigot.jar nogui
+java -Xmx${HEAP} -Xms${HEAP} -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -Dcom.mojang.eula.agree=true -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true -jar /opt/minecraft/spigot.jar nogui
